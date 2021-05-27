e2c5dd361cd5488c55fa1bc4475e5ac0837249d9
Nicolás Oliva <oliva.nicolas30@gmail.com>
2021-05-27 05:43:18 -0400

    config native-libray from GeoLocation to android

--- App.js
+++ App.js
@@ -1,9 +1,58 @@
-
-import React from 'react'
-import Root from './src/routes/Root'
+import React, { useState, useEffect } from 'react';
+import { PermissionsAndroid, Platform } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import GeoLocation from '@react-native-community/geolocation';
+import Root from './src/routes/Root';
 
 export default App = () => {
+
+  const [currentLatitude, setCurrentLatitude] = useState('')
+  const [currentLongitude, setCurrentLongitude] = useState('')
+
+  const getcurrentLocation = () => {
+    GeoLocation.getCurrentPosition((position) => {
+      let currentLatitude = JSON.stringify(position.coords.latitude)
+      let currentLongitude = JSON.stringify(position.coords.longitude)
+      setCurrentLatitude(currentLatitude)
+      setCurrentLongitude(currentLongitude)
+    }, (error) => alert(error.message), {
+      enableHighAccuracy: true, timeout: 20000, maximumAge: 1000
+    })
+  }
+
+//Esta funcion se crea para versiones de sdk menor al 23, superior a ella la ubicación se proporciona automaticamente.
+  const callLocation = () => { 
+    if (Platform.OS === 'ios') {
+      getcurrentLocation()
+    } else {
+      const requesPermission = async () => {
+        const granted = await PermissionsAndroid.request( 
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          {
+            title: 'Permisos de localización',
+            message: 'La aplicación necesita la ubicacion actual',
+            buttonNegative: 'cancelar',
+            buttonPositive: 'Aceptar',
+            buttonNeutral: 'En otro momento'
+          }
+        )
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          getcurrentLocation()
+        } else {
+          alert('Permisos denegados')
+        }
+      }
+      requesPermission();
+    }
+  }
+
+  useEffect(() => {
+    callLocation()
+    return () => {
+
+    }
+  }, [])
+
   return (
     <NavigationContainer>
       <Root />
--- android/app/src/main/java/com/indicatorsrn/MainApplication.java
+++ android/app/src/main/java/com/indicatorsrn/MainApplication.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.reactnativecommunity.geolocation.GeolocationPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
           //packages.add(new VectorIconsPackage());
+         // packages.add(new GeolocationPackage());
           return packages;
         }
 
